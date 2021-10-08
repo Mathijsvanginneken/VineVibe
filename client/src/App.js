d257d97c04d54c51b292bc9d819da6ef4d01fbdb
Mathijsvanginneken <mathijsvginneken@gmail.com>
2021-10-08 12:58:58 +0200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import WineDetailsPage from './Pages/WineDetailsPage';
 import createWine from './Pages/createWine';
 import WineMap from './Pages/WineMap';
 import ErrorPage404 from './Pages/ErrorPage404';
-import ErrorPage500 from './Pages/ErrorPage500';
 import { Switch, Route } from 'react-router-dom';
 import AboutUs from './Pages/AboutUs';
 import 'bulma/css/bulma.min.css';
@@ -32,17 +31,16 @@ function App() {
       <div className="columns is-centered">
         <div className="column is-10">
           <Switch>
-            <Route exact path="/login" component={UserLogin} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/user" component={UserProfile} />
-            <Route exact path="/create-wine" component={createWine} />
-            <Route exact path="/winemap" component={WineMap} />
-            <Route exact path="/wine/:id" component={WineDetailsPage} />
-            <Route exact path="/search" component={SearchWines} />
-            <Route exact path="/about" component={AboutUs} />
-            <Route exact path="/user/:userId" component={EditProfile} />
-            <Route exact path="/404" component={ErrorPage404} />
-            <Route exact path="/500" component={ErrorPage500} />
+            <Route path="/login" component={UserLogin} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/user" component={UserProfile} />
+            <Route path="/create-wine" component={createWine} />
+            <Route path="/winemap" component={WineMap} />
+            <Route path="/wine/:id" component={WineDetailsPage} />
+            <Route path="/search" component={SearchWines} />
+            <Route path="/about" component={AboutUs} />
+            <Route path="/user/:userId" component={EditProfile} />
+            <Route path="/404" component={ErrorPage404} />
             <Route exact path="/" component={HomePage} />
           </Switch>
         </div>
--- client/src/Pages/ErrorPage404.js
+++ client/src/Pages/ErrorPage404.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import errorImage from "../Images/errorImage.png"
+
 
 export default function ErrorPage404() {
     return (
-        <div>
-            <h1>Error 404</h1>
+        <div className="error-page">  
+        <h1 className="errorText">Error 404</h1>
+        <img src={errorImage} alt="404" className="errorImage2"/>
         </div>
     )
 }
